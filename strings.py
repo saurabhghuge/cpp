c20685fcf226cf0fb21697513935223b5f8b54d6
Saurabh Ghuge <saurabhghuge1998@gmail.com>
2022-08-22 22:51:53 +0530

    minions game

---
+++ strings.py
@@ -0,0 +1,21 @@
+ def minion_game(string):
+     
+    def stuart(s):
+        
+      # your code goes here
+def subString(s, n): 
+    # Pick starting point in outer loop 
+    # and lengths of different strings for 
+    # a given starting point 
+    du = []
+    for i in range(n): 
+        for len in range(i+1,n+1): 
+            print(du = s[i: len]); 
+  
+# Driver program to test above function 
+     
+    
+if __name__ == '__main__':
+    s = input()
+    subString(s,len(s))
+    minion_game(s)
